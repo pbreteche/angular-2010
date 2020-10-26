155a1afaecbff414d67572f8d8790a8e3c966055
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-26 17:31:39 +0100

    add data to component, make a loop

---
+++ src/mock/app.data.ts
@@ -0,0 +1,6 @@
+export const todolist = [
+  { title: 'voir Typescript', deadline: '2020-10-26' },
+  { title: 'découvrir l\'architecture Angular', deadline: '2020-10-26' },
+  { title: 'Les composants en détail', deadline: '2020-10-27' },
+  { title: 'Le routing', deadline: '2020-10-27' },
+];
