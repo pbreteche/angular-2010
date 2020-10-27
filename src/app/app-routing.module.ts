ef1926a3b229824c5e7eb575378cba435d47d8d6
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-27 12:09:34 +0100

    first static nav menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TodoNavComponent} from './todo-nav/todo-nav.component';
+import {TodoCreateFormComponent} from './todo-create-form/todo-create-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodoNavComponent },
+  { path: 'new', component: TodoCreateFormComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
