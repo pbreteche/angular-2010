a1d431d1ffe9c9adf199043f37a01d9e5028bca1
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-27 12:58:25 +0100

    get route params

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,12 +1,24 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {Todo, todolist} from '../../mock/app.data';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.scss']
 })
-export class TodoDetailComponent {
+export class TodoDetailComponent implements OnInit {
   @Input()
   todo: Todo = todolist[0];
+
+  constructor(private route: ActivatedRoute, private title: Title) {
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(map => {
+      this.todo = todolist[map.get('id')];
+      this.title.setTitle(this.todo.title);
+    });
+  }
 }
