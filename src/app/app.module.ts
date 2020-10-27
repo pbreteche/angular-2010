ca1cc6f10745759221485e2e802b06f74a809d51
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-27 09:46:34 +0100

    ng generate component todo-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDetailComponent
   ],
   imports: [
     BrowserModule
