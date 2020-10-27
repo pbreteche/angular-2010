7607cf345112ca66c361eb00a40a7b911ccd9d34
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-27 09:54:57 +0100

    property binding between components

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Todo, todolist} from '../../mock/app.data';
 
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.scss']
 })
-export class TodoDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TodoDetailComponent {
+  @Input()
+  todo: Todo = todolist[0];
 }
