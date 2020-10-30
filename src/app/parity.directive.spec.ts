cd1c6a94d685fb99f53ee82145bf4e126093f96c
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-30 15:27:04 +0100

    test attribute directive

--- src/app/parity.directive.spec.ts
+++ src/app/parity.directive.spec.ts
@@ -1,8 +1,27 @@
 import { ParityDirective } from './parity.directive';
+import {TestBed} from '@angular/core/testing';
+import {Component, DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
+
+@Component({
+  template: '<p [appParity]="1"></p><p [appParity]="2"></p>'
+})
+class TestComponent {}
 
 describe('ParityDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ParityDirective();
-    expect(directive).toBeTruthy();
+  let p: DebugElement[];
+
+  beforeEach(() => {
+    const fixture = TestBed.configureTestingModule({
+      declarations: [ParityDirective, TestComponent]
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges();
+
+    p = fixture.debugElement.queryAll(By.css('p'));
+  });
+
+  it('should add "odd" class to first paragraph', () => {
+    expect(p[0].nativeElement.classList.has('odd')).toBeTruthy();
   });
 });
