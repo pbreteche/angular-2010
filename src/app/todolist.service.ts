f004c3b43bf743c519774542735182aa871bd9e1
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-30 10:41:42 +0100

    post and put request

--- src/app/todolist.service.ts
+++ src/app/todolist.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Todo} from './todo';
-import {HttpClient} from '@angular/common/http';
-import {BehaviorSubject, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -35,12 +35,37 @@ export class TodolistService {
   update(id: number, todo: Todo): void {
     const todos = this.subject.getValue();
     todos[id] = todo;
+    this.http.put('todo/' + id, todo, {
+      headers: {
+        Accept: 'application/json',
+      }
+    }).pipe(
+      catchError(this.handleError)
+    ).subscribe(successResponse => console.log(successResponse));
     this.subject.next(todos);
   }
 
   add(newTodo: Todo): void {
     const todos = this.subject.getValue();
     todos.push(newTodo);
+    this.http.post('todo', newTodo).pipe(
+      catchError(this.handleError)
+    ).subscribe(successResponse => console.log(successResponse));
     this.subject.next(todos);
+  }
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
